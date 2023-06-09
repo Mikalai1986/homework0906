ada316125aacb40cfb56d6d509608019a5252388
FostikN <fostik@tut.by>
2023-06-09 13:55:29 +0300

    выполнил первое задание

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Homework
+/* Задача2: Напишите программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше
+*/
+Console.Write("Ведите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ведите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2)
+    Console.Write( number1 +  " равно " + number2);
+    else
+        if (number1 > number2)
+         Console.Write( number1 +  " max ");
+    else
+    Console.Write(number2 +" max ");    
