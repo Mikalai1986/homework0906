a9289a449ecdf47342c0a6070e3cd07520e58dc8
FostikN <fostik@tut.by>
2023-06-09 15:28:26 +0300

    выполнил 4-е задание

--- Program.cs
+++ Program.cs
@@ -32,10 +32,20 @@ if (number3 > max)
 Console.WriteLine(number1 + ", " + number2 + ", " + number3 + "->" + max);
  Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным 
 (делится ли оно на два без остатка)
-*/
+
  Console.Write("Ведите число:");
 int num = Convert.ToInt32(Console.ReadLine());
 int del = num % 2;
 if (del == 0)
     Console.WriteLine("Число "+ num + " - четное");
 else Console.WriteLine("Число "+ num + " - нечетное");
+Задача 8: Напишите программу, которая на вход принимает число N, а на выходе показывает все четные числа 
+от 1 до N
+*/
+Console.WriteLine("Введите число N");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= num)
+    {Console.Write(count + ", ");
+    count=count+2;}
+//Console.Write(count);
